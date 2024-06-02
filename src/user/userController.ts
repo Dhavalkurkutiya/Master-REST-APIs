9180cf40adc5944bef3bb38061d34dfab2f3e7a9
Dhaval Kurkutuiya <Kurkutiyadhaval30@gmail.com>
2024-06-02 12:19:09 +0530

    jwt token sign

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
 
@@ -12,21 +14,29 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // Check if user already exists
-    const user = await userModel.findOne({email});
+    const user = await userModel.findOne({ email });
     if (user) {
         const error = createHttpError(400, "User already exists");
         return next(error);
     }
 
-   // Passwor hashing
-const hashedPassword = await bcrypt.hash(password, 12);
+    // Passwor hashing
+    const hashedPassword = await bcrypt.hash(password, 12);
 
+    const newUSer = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    // Token ganeration
+    const token = sign({ sub: newUSer._id }, config.jwtSecret as string,{ expiresIn: "1h" , algorithm: "HS256"});
 
     // Processing
 
     // Response
 
-    res.json({ message: "User created" });
+    res.json({ axxessToken: token});
 };
 
 export { createUser };
