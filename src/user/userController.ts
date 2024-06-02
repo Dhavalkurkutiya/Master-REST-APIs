963250d9e9a8551b46d86886513e3bf54fcbdc69
Dhaval Kurkutuiya <Kurkutiyadhaval30@gmail.com>
2024-06-02 12:30:30 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
 
@@ -13,30 +14,46 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return next(error);
     }
 
-    // Check if user already exists
-    const user = await userModel.findOne({ email });
-    if (user) {
-        const error = createHttpError(400, "User already exists");
-        return next(error);
+    try {
+        // Check if user already exists
+        const user = await userModel.findOne({ email });
+        if (user) {
+            const error = createHttpError(400, "User already exists");
+            return next(error);
+        }
+    } catch (error) {
+        return next(createHttpError(500, "Already exists check failed"));
     }
 
     // Passwor hashing
+
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const newUSer = await userModel.create({
-        name,
-        email,
-        password: hashedPassword,
-    });
+    let newUSer: User;
+    try {
+        newUSer = await userModel.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+    } catch (error) {
+        return next(createHttpError(500, "Password hashing failed"));
+    }
 
-    // Token ganeration
-    const token = sign({ sub: newUSer._id }, config.jwtSecret as string,{ expiresIn: "1h" , algorithm: "HS256"});
+    try {
+        // Token ganeration
+        const token = sign({ sub: newUSer._id }, config.jwtSecret as string, {
+            expiresIn: "1h",
+            algorithm: "HS256",
+        });
+        res.json({ axxessToken: token });
+    } catch (error) {
+        return next(createHttpError(500, "Generating token failed"));
+    }
 
     // Processing
 
     // Response
-
-    res.json({ axxessToken: token});
 };
 
 export { createUser };
